c0e2d2ec0de4ea0a425a26803396af900dc33859
Artak Mkrtchyan <artak@outlook.com>
2019-05-31 21:29:55 -0700

    Added the `HostBuilderExtensions` to configure HostBuilder with default configuration.

---
+++ src/Hosting/src/HostBuilderExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.IO;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// Encapsulates a set of extension methods for <see cref="HostBuilder"/> type. 
+    /// </summary>
+    public static class HostBuilderExtensions
+    {
+        /// <summary>
+        /// Applies an oppiniated default logging and configuration settings to the host builder.
+        /// </summary>
+        /// <param name="hostBuilder">The host builder to configure.</param>
+        /// <returns>The passed in instance.</returns>
+        /// <remarks>
+        /// The default configuration accepts settings through `appsettings[.environmentName].json` file(s).
+        /// By default `Console` and `Debug` logging providers are configured.
+        /// </remarks>
+        public static HostBuilder ConfigureDefaults(this HostBuilder hostBuilder)
+        {
+            hostBuilder
+                .ConfigureAppConfiguration((hostContext, configApp) =>
+                {
+                    configApp.SetBasePath(Directory.GetCurrentDirectory());
+                    configApp.AddJsonFile("appsettings.json", optional: true);
+                    configApp.AddJsonFile(
+                        $"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json",
+                        optional: true);
+                })
+                .ConfigureLogging((hostContext, configLogging) =>
+                {
+                    configLogging.AddConsole();
+                    configLogging.AddDebug();
+                })
+                .UseConsoleLifetime();
+
+            return hostBuilder;
+        }
+    }
+}
--- src/Hosting/src/TimedHostedService.cs
+++ src/Hosting/src/TimedHostedService.cs
@@ -8,20 +8,33 @@ using System.Threading.Tasks;
 
 namespace AM.AspNetCore.Hosting
 {
-    internal abstract class TimedHostedServiceBase : IHostedService, IDisposable
+    public abstract class TimedHostedServiceBase : IHostedService, IDisposable
     {
         private Timer _timer;
         private Task _lastTask = Task.CompletedTask;
         private readonly IOptionsMonitor<TimeHostedServiceOptions> _periodMonitor;
 
+        /// <summary>
+        /// Gets the configured logger instance.
+        /// </summary>
         protected ILogger Logger { get; }
 
+        /// <summary>
+        /// Instantiates the object being created using the passed in option and logger.
+        /// </summary>
+        /// <param name="periodMonitor">The configuration options for the periodic execution</param>
+        /// <param name="logger">The logger instance.</param>
         protected TimedHostedServiceBase(IOptionsMonitor<TimeHostedServiceOptions> periodMonitor, ILogger logger)
         {
             Logger = logger;
             _periodMonitor = periodMonitor.Ensure(nameof(periodMonitor)).IsNotNull().Value;
         }
 
+        /// <summary>
+        /// Starts the timed hosted service. This method is being called by the host.
+        /// </summary>
+        /// <param name="cancellationToken">A cancellation token to be used for interrupting execution</param>
+        /// <returns>An asynchronously executing task tracking the `Start` request for this service instance.</returns>
         public Task StartAsync(CancellationToken cancellationToken)
         {
             Logger.LogInformation("Timed Background Service is starting.");
@@ -32,17 +45,11 @@ namespace AM.AspNetCore.Hosting
             return Task.CompletedTask;
         }
 
-        private void OnTimerElapsed(object state)
-        {
-            if (!this._lastTask.IsCompleted)
-                return;
-
-            Logger.LogInformation("Timed Background Service is resuming to run periodic task.");
-            _lastTask = this.DoWorkAsync(state);
-        }
-
-        protected abstract Task DoWorkAsync(object state);
-
+        /// <summary>
+        /// Stops the timed hosted service. This method is being called by the host.
+        /// </summary>
+        /// <param name="cancellationToken">A cancellation token to be used for interrupting execution</param>
+        /// <returns>An asynchronously executing task tracking the `Stop` request for this service instance.</returns>
         public async Task StopAsync(CancellationToken cancellationToken)
         {
             Logger.LogInformation("Timed Background Service is stopping.");
@@ -52,9 +59,40 @@ namespace AM.AspNetCore.Hosting
             await _lastTask;
         }
 
+        /// <summary>
+        /// Disposes the service instance.
+        /// </summary>
         public void Dispose()
         {
-            _timer?.Dispose();
+            this.Dispose(true);
+        }
+
+        /// <summary>
+        /// Implementing classes should override this method if they need to dispose of other disposable resources.
+        /// </summary>
+        /// <param name="disposing">A boolean value indicating whether the instance is being disposed.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _timer?.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// Child classes must override this method to provide custom logic to be run when the service being invoked.
+        /// </summary>
+        /// <param name="state"></param>
+        /// <returns></returns>
+        protected abstract Task DoWorkAsync(object state);
+
+        private void OnTimerElapsed(object state)
+        {
+            if (!this._lastTask.IsCompleted)
+                return;
+
+            Logger.LogInformation("Timed Background Service is resuming to run periodic task.");
+            _lastTask = this.DoWorkAsync(state);
         }
     }
 }
